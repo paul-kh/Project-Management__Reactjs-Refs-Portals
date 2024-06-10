2150b61efd15e81fc161ac4815cd971c49fb2855
Paul Chheang <phalcheat@gmail.com>
2024-06-10 17:39:09 -0500

    Created state for switching between components <NoProjectSelected> and <NewProject>

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,40 @@ import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
+import { useState } from "react";
+
 function App() {
+  /************************************************************************************ 
+    The projectState object is used to display UI when user starts adding new project
+    or no project is selected:
+    - projectsState.selectedProjectId = undefined ==> if no project is selected
+    - projectsState.selectedProjectId = null      ==> if user started adding new project 
+  *************************************************************************************/
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
