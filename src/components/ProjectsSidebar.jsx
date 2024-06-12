534fd26ba1ab658e41716019387aae3c1d2181a4
Paul Chheang <phalcheat@gmail.com>
2024-06-11 22:17:03 -0500

    Dynamically styled the Selected Project once user selected it

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -20,16 +20,15 @@ export default function ProjectsSidebar({
           let cssClasses =
             "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800";
 
-          if (project.projectId === seletedProjectId) {
-            cssClasses += " bg-stone-800 text-stone-200";
-          } else {
-            cssClasses += " text-stone-400";
-          }
           return (
             <li key={project.id}>
               <button
                 onClick={() => onSelectProject(project.id)}
-                className={cssClasses}
+                className={
+                  project.id === seletedProjectId
+                    ? (cssClasses += " bg-stone-800 text-stone-200")
+                    : (cssClasses += " text-stone-400")
+                }
               >
                 {project.title}
               </button>
